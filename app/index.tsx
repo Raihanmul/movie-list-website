6152accee21bc0c87ee552c1fb8cc32996e45f97
Raihanmul <raihanmul10@gmail.com>
2025-12-02 15:21:48 +0700

    fetch api key

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,62 @@
-import { Text, View } from "react-native";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { FlatList, Text, View } from "react-native";
 
-export default function Index() {
+type Movie = {
+  Title: string;
+  Year: string;
+  imdbID: string;
+  Type: string;
+  Poster: string;
+};
+
+export default function HomeScreen() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchMovies = async () => {
+    setLoading(true);
+
+    try {
+      const res = await axios.get(
+        "https://www.omdbapi.com/?apikey=fa0611ea&s=boboiboy&page=1"
+      );
+
+      if (res.data.Search) {
+        setMovies(res.data.Search as Movie[]);
+      }
+    } catch (error) {
+      console.log("Error fetching movies:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        paddingTop: 50,
+        paddingHorizontal: 20,
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <FlatList
+          data={movies}
+          keyExtractor={(item) => item.imdbID}
+          renderItem={({ item }) => (
+            <Text style={{ fontSize: 18, marginBottom: 6 }}>
+              {item.Title} ({item.Year})
+            </Text>
+          )}
+        />
+      )}
     </View>
   );
 }
